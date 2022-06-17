0eac806ab2b1f225d26ddb3565093b9cefed9b32
Aarif <03aarif20@gmail.com>
2022-06-17 14:54:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model1.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index2.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    # output = round(prediction[0], 2)
+
+    return render_template('index2.html', prediction_text=' bond strength{} KN'.format(int(prediction)))
+
+if __name__ == "__main__":
+    app.run(debug=True)
